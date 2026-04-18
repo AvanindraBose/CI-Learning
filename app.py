23a931da41c3108288595cbd6f84c5ff99c81ea3
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-18 07:09:28 +0530

    Fixed Linting

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def subtract(a, b):
+    return a - b
 
 
+result1 = add(5, 3)
+result2 = subtract(5, 3)
 
 
-def subtract(a,b):return a-b
-
-
-result1=add(5,3)
-result2=subtract(5,3)
-
-print("Addition:",result1)
-print("Subtraction:",result2)
+print("Addition:", result1)
+print("Subtraction:", result2)
