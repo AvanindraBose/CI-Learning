f0f26170577425ef6932b51962fa1f63ddb49ea6
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-18 08:04:35 +0530

    Implemented the test logic

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ def multiply(a, b):
     return a * b
 
 
-result1 = add(5, 3)
-result2 = subtract(5, 3)
+if __name__ == "__main__":
+    result1 = add(5, 3)
+    result2 = subtract(5, 3)
+    result3 = multiply(5, 3)
 
-
-print("Addition:", result1)
-print("Subtraction:", result2)
+    print("Addition:", result1)
+    print("Subtraction:", result2)
+    print("Multiplication:", result3)
